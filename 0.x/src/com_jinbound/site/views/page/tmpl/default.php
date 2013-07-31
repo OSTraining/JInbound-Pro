730ff8543dd64474245f3953ee6897163eb0fdce
Jeff Channell <github@jeffchannell.com>
2013-07-31 15:09:08 -0400

    Removed debug info from frontend page view

--- 0.x/src/com_jinbound/site/views/page/tmpl/default.php
+++ 0.x/src/com_jinbound/site/views/page/tmpl/default.php
@@ -10,5 +10,7 @@ defined('JPATH_PLATFORM') or die;
 echo $this->loadTemplate('layout');
 
 ?>
+<?php if (defined('JDEBUG') && JDEBUG) : ?>
 <pre><?php echo htmlspecialchars(print_r($this->item, 1)); ?></pre>
 <pre><?php echo htmlspecialchars(print_r($this->form, 1)); ?></pre>
+<?php endif; ?>
