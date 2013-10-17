30d727e98b212a03db95834a0134ac83947374c4
Jeff Channell <github@jeffchannell.com>
2013-10-17 14:16:05 -0400

    Changed social icons template to load module position "jinbound_social" for #42

--- 0.x/src/com_jinbound/site/views/page/tmpl/default_social.php
+++ 0.x/src/com_jinbound/site/views/page/tmpl/default_social.php
@@ -7,5 +7,14 @@
 
 defined('JPATH_PLATFORM') or die;
 
-?>
-TODO: social media buttons
+// render a module position here
+$modules = JModuleHelper::getModules('jinbound_social');
+
+if (!empty($modules)) {
+	foreach ($modules as $module) {
+		$mparams = new JRegistry;
+		$mparams->loadString($module->params);
+		echo JModuleHelper::renderModule($module, $mparams->toArray());
+	}
+}
+
