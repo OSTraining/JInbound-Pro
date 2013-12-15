2216c66a68a24f4f8e71391803c28f5bab7d45c9
jeffchannell <github@jeffchannell.com>
2013-12-15 13:59:59 -0500

    Fixes #117

---
+++ 0.x/src/com_jinbound/admin/models/fields/jinboundcronurl.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package		JInbound
+ * @subpackage	com_jinbound
+ @ant_copyright_header@
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+class JFormFieldJinboundCronUrl extends JFormField
+{
+	protected $type = 'JinboundCronUrl';
+	
+	/**
+	 * output a link
+	 * 
+	 * (non-PHPdoc)
+	 * @see JFormField::getInput()
+	 */
+	protected function getInput() {
+		$url = JURI::root() . 'index.php?option=com_jinbound&task=cron';
+		return '<a href="' . $url . '" target="_blank">' . $url . '</a>';
+	}
+}
