313b0bb961354094123534e08035a7cdd949fd79
jeffchannell <github@jeffchannell.com>
2013-12-16 15:01:16 -0500

    Adjusted template handing in menu item form #116

--- 0.x/src/com_jinbound/media/js/admin.menu.js
+++ 0.x/src/com_jinbound/media/js/admin.menu.js
@@ -30,6 +30,36 @@ window.addEvent('load', function(){
 		}
 	}
 	if (template_id != select.options[select.selectedIndex].value && document.getElementById('jform_link').getAttribute('value').toString().match(/^index.php\?option=com_jinbound/)) {
-		alert(Joomla.JText._('COM_JINBOUND_MENU_NOT_SET_TO_USE_JINBOUND_TEMPLATE'));
+		var id_input  = document.getElementById('jform_id');
+		var form      = document.getElementById('item-form');
+		var id_action = false;
+		if (form) {
+			var action_vars = form.getAttribute('action').split('?')[1].split('&')
+			for (var i = 0; i < action_vars.length; i++) {
+				var pair = action_vars[i].split("=");
+				if ('id' == pair[0]) {
+					if (0 < parseInt(pair[1],10)) {
+						id_action = parseInt(pair[1],10);
+					}
+					break;
+				}
+			}
+		}
+		if ((id_input && 0 < id_input.value) || (id_action)) {
+			alert(Joomla.JText._('COM_JINBOUND_MENU_NOT_SET_TO_USE_JINBOUND_TEMPLATE'));
+		}
+		else {
+			console.log(select);
+			console.log(select.value);
+			select.value = template_id;
+			try {
+				jQuery('#jform_template_style_id').trigger('liszt:updated');
+			}
+			catch (err) {
+				// do nothing, probably 2.5
+			}
+			console.log(select);
+			console.log(select.value);
+		}
 	}
 });
