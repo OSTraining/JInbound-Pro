ac391fda9552f5d9c504d0d933cb3eb41edc542e
jeffchannell <github@jeffchannell.com>
2014-12-02 16:19:53 -0500

    Added enctype to page form to allow file uploads

--- 0.x/src/com_jinbound/site/views/page/tmpl/default_form_open.php
+++ 0.x/src/com_jinbound/site/views/page/tmpl/default_form_open.php
@@ -8,4 +8,4 @@
 defined('JPATH_PLATFORM') or die;
 
 ?>
-		<form action="<?php echo JInboundHelperUrl::task('lead.save', true, array('page_id' => (int) $this->item->id)); ?>" method="post">
+		<form action="<?php echo JInboundHelperUrl::task('lead.save', true, array('page_id' => (int) $this->item->id)); ?>" method="post" enctype="multipart/form-data">
