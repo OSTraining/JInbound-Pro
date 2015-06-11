f1ced8c6db6754b3a14c8a5d9ae6f5078810fb7a
jeffchannell <github@jeffchannell.com>
2015-06-11 11:14:24 -0400

    Corrected file headers

--- 0.x/src/plugins/system/jinboundfiles/field/jinboundfilesupdate.php
+++ 0.x/src/plugins/system/jinboundfiles/field/jinboundfilesupdate.php
@@ -1,8 +1,8 @@
 <?php
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @package		JInbound
+ * @subpackage	plg_system_jinboundfiles
+@ant_copyright_header@
  */
 
 defined('_JEXEC') or die;
