805bb15289d2f4d30d84190d2417c030663e067c
Bill Tomczak <bill@ostraining.com>
2018-03-08 11:42:49 -0800

    Update all file headers

--- src/plugins/content/jinbound/jinbound.php
+++ src/plugins/content/jinbound/jinbound.php
@@ -2,7 +2,17 @@
 /**
  * @package             JInbound
  * @subpackage          plg_content_jinbound
- * @ant_copyright_header@
+ **********************************************
+ * JInbound
+ * Copyright (c) 2013 Anything-Digital.com
+ * Copyright (c) 2018 Open Source Training, LLC
+ **********************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.n *
+ * This header must not be removed. Additional contributions/changes
+ * may be added to this header as long as no information is deleted.
  */
 
 defined('JPATH_PLATFORM') or die;
