06aa6955eba2f08d0cbb27eda8d07d31bd4b09ec
Bill Tomczak <bill@ostraining.com>
2018-03-12 12:02:15 -0700

    Simplify/deprecate asset table class override

--- src/admin/libraries/tables/asset.php
+++ src/admin/libraries/tables/asset.php
@@ -22,43 +22,38 @@ JInbound::registerLibrary('JinboundTable', 'table');
 
 /**
  * This is a base class for backwards compat
+ *
+ * @deprecated v2.1.8
  */
-class JInboundBaseAssetTable extends JInboundTable
+class JInboundAssetTable extends JInboundTable
 {
     public $asset_id;
 
     /**
      * Our compat method
      *
-     * @param unknown_type $table
-     * @param unknown_type $id
+     * @param JTable $table
+     * @param string $id
+     *
+     * @return int
+     * @deprecated v2.1.8
      */
-    protected function _compat_getAssetParentId($table = null, $id = null)
+    protected function _compat_getAssetParentId(JTable $table = null, $id = null)
     {
+        /** @var JTableAsset $asset */
         $asset = JTable::getInstance('Asset');
         $asset->loadByName('com_jinbound');
         return $asset->id;
     }
-}
 
-/**
- * Declare the shim class that defines _getAssetParentId in different ways based on version
- *
- */
-if (JInbound::version()->isCompatible('3.2.0')) {
-    class JInboundAssetTable extends JInboundBaseAssetTable
+    /**
+     * @param JTable $table
+     * @param string $id
+     *
+     * @return int
+     */
+    protected function _getAssetParentId(JTable $table = null, $id = null)
     {
-        protected function _getAssetParentId(JTable $table = null, $id = null)
-        {
-            return $this->_compat_getAssetParentId($table, $id);
-        }
-    }
-} else {
-    class JInboundAssetTable extends JInboundBaseAssetTable
-    {
-        protected function _getAssetParentId($table = null, $id = null)
-        {
-            return $this->_compat_getAssetParentId($table, $id);
-        }
+        return $this->_compat_getAssetParentId($table, $id);
     }
 }
