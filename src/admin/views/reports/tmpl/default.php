d83cc1810557ea26fc3114f2e8b9851d73a5cecf
Bill Tomczak <bill@ostraining.com>
2018-03-16 14:16:49 -0700

    Cleanup main reports template

--- src/admin/views/reports/tmpl/default.php
+++ src/admin/views/reports/tmpl/default.php
@@ -15,90 +15,140 @@
  * may be added to this header as long as no information is deleted.
  */
 
+use Joomla\Utilities\ArrayHelper;
+
 defined('JPATH_PLATFORM') or die;
 
 JHtml::_('behavior.calendar');
 
 ?>
-<script type="text/javascript">
-    Joomla.submitbutton = function(task) {
-        console.log('submitbutton');
-        console.log(task);
-        Joomla.submitform(task, document.getElementById('adminForm'));
-    };
-</script>
-<div class="container-fluid <?php echo $this->viewClass; ?>" id="jinbound_component">
-    <?php if (!empty($this->sidebar)) : ?>
-    <div id="j-sidebar-container" class="span2">
-        <?php echo $this->sidebar; ?>
-    </div>
-    <div id="j-main-container" class="span10">
-        <?php else : ?>
-        <div id="j-main-container">
-            <?php endif; ?>
+    <script type="text/javascript">
+        Joomla.submitbutton = function(task) {
+            Joomla.submitform(task, document.getElementById('adminForm'));
+        };
+    </script>
+    <div class="container-fluid <?php echo $this->viewClass; ?>" id="jinbound_component">
+        <?php
+        $mainAttributes = array(
+            'id' => 'j-main-container'
+        );
+        if (!empty($this->sidebar)) :
+            $mainAttributes['class'] = 'span10';
+            ?>
+            <div id="j-sidebar-container" class="span2">
+                <?php echo $this->sidebar; ?>
+            </div>
+        <?php
+        endif;
+        ?>
+        <div <?php echo ArrayHelper::toString($mainAttributes); ?>
+        <?php
+        echo JHtml::_('jinbound.startTabSet', 'jinbound_default_tabs', array('active' => 'content_tab'));
 
-            <?php echo JHtml::_('jinbound.startTabSet', 'jinbound_default_tabs', array('active' => 'content_tab')); ?>
-            <?php echo JHtml::_('jinbound.addTab', 'jinbound_default_tabs', 'content_tab',
-                JText::_('COM_JINBOUND_REPORTS', true)); ?>
-            <form action="<?php echo JInboundHelperUrl::view('reports'); ?>" method="post" id="adminForm"
-                  name="adminForm" class="form-validate" enctype="multipart/form-data">
-                <div class="row-fluid">
-                    <div class="span12 text-center">
-                        <div class="reports_search">
-                            <?php echo $this->campaign_filter; ?>
-                            <?php echo $this->page_filter; ?>
-                            <?php echo $this->priority_filter; ?>
-                            <?php echo $this->status_filter; ?>
-                            <?php echo JHtml::_('calendar', $this->state->get('filter_start'), 'filter_start',
-                                'filter_start', '%Y-%m-%d', array(
-                                    'size'        => 10
-                                ,
-                                    'placeholder' => JText::_('COM_JINBOUND_FROM')
-                                ,
-                                    'onchange'    => $this->filter_change_code
-                                )); ?>
-                            <?php echo JHtml::_('calendar', $this->state->get('filter_end'), 'filter_end', 'filter_end',
-                                '%Y-%m-%d', array(
-                                    'size'        => 10
-                                ,
-                                    'placeholder' => JText::_('COM_JINBOUND_TO')
-                                ,
-                                    'onchange'    => $this->filter_change_code
-                                )); ?>
-                        </div>
+        echo JHtml::_(
+            'jinbound.addTab',
+            'jinbound_default_tabs',
+            'content_tab',
+            JText::_('COM_JINBOUND_REPORTS', true)
+        );
+        ?>
+        <form action="<?php echo JInboundHelperUrl::view('reports'); ?>"
+              method="post"
+              id="adminForm"
+              name="adminForm"
+              class="form-validate"
+              enctype="multipart/form-data">
+            <div class="row-fluid">
+                <div class="span12 text-center">
+                    <div class="reports_search">
+                        <?php
+                        echo $this->campaign_filter;
+                        echo $this->page_filter;
+                        echo $this->priority_filter;
+                        echo $this->status_filter;
+
+                        echo JHtml::_(
+                            'calendar',
+                            $this->state->get('filter_start'),
+                            'filter_start',
+                            'filter_start',
+                            '%Y-%m-%d',
+                            array(
+                                'size'        => 10,
+                                'placeholder' => JText::_('COM_JINBOUND_FROM'),
+                                'onchange'    => $this->filter_change_code
+                            )
+                        );
+
+                        echo JHtml::_(
+                            'calendar',
+                            $this->state->get('filter_end'),
+                            'filter_end',
+                            'filter_end',
+                            '%Y-%m-%d',
+                            array(
+                                'size'        => 10,
+                                'placeholder' => JText::_('COM_JINBOUND_TO'),
+                                'onchange'    => $this->filter_change_code
+                            )
+                        );
+                        ?>
                     </div>
                 </div>
-                <?php echo $this->loadTemplate('dashboard'); ?>
-                <div>
-                    <input name="task" value="" type="hidden"/>
-                </div>
-            </form>
+            </div>
 
-            <?php echo JHtml::_('jinbound.endTab'); ?>
-            <?php if ($this->permissions && JFactory::getUser()->authorise('core.admin', JInbound::COM)) : ?>
-                <?php echo JHtml::_('jinbound.addTab', 'jinbound_default_tabs', 'permissions_tab',
-                    JText::_('JCONFIG_PERMISSIONS_LABEL', true)); ?>
-                <div class="row-fluid">
-                    <form action="<?php echo JRoute::_('index.php?option=com_jinbound&task=reports.permissions'); ?>"
-                          method="post">
-                        <?php foreach ($this->permissions->getFieldsets() as $fieldset) : ?>
-                            <?php $fields = $this->permissions->getFieldset($fieldset->name); ?>
-                            <fieldset>
-                                <legend><?php echo JText::_($fieldset->label); ?></legend>
-                                <?php foreach ($fields as $field) : ?>
-                                    <?php echo $field->input; ?>
-                                <?php endforeach; ?>
-                            </fieldset>
-                        <?php endforeach; ?>
-                        <?php echo JHtml::_('form.token'); ?>
-                        <button type="submit" class="btn btn-primary"><i
-                                class="icon-save"></i> <?php echo JText::_('JTOOLBAR_APPLY'); ?> </button>
-                    </form>
-                </div>
-                <?php echo JHtml::_('jinbound.endTab'); ?>
-            <?php endif; ?>
-            <?php echo JHtml::_('jinbound.endTabSet'); ?>
-        </div>
+            <?php echo $this->loadTemplate('dashboard'); ?>
+            <div>
+                <input name="task" value="" type="hidden"/>
+            </div>
+        </form>
+
+        <?php
+        echo JHtml::_('jinbound.endTab');
+
+        if ($this->permissions && JFactory::getUser()->authorise('core.admin', 'com_jinbound')) :
+            echo JHtml::_(
+                'jinbound.addTab',
+                'jinbound_default_tabs',
+                'permissions_tab',
+                JText::_('JCONFIG_PERMISSIONS_LABEL', true)
+            );
+            ?>
+            <div class="row-fluid">
+                <form action="<?php echo JRoute::_('index.php?option=com_jinbound&task=reports.permissions'); ?>"
+                      method="post">
+                    <?php
+                    foreach ($this->permissions->getFieldsets() as $fieldset) :
+                        $fields = $this->permissions->getFieldset($fieldset->name);
+                        ?>
+                        <fieldset>
+                            <legend><?php echo JText::_($fieldset->label); ?></legend>
+                            <?php foreach ($fields as $field) : ?>
+                                <?php echo $field->input; ?>
+                            <?php endforeach; ?>
+                        </fieldset>
+                    <?php
+                    endforeach;
+
+                    echo JHtml::_('form.token');
+                    ?>
+                    <button type="submit"
+                            class="btn btn-primary">
+                        <i class="icon-save"></i>
+                        <?php echo JText::_('JTOOLBAR_APPLY'); ?>
+                    </button>
+                </form>
+            </div>
+            <?php
+            echo JHtml::_('jinbound.endTab');
+
+        endif;
+
+        echo JHtml::_('jinbound.endTabSet');
+        ?>
     </div>
-    <?php echo $this->loadTemplate('footer'); ?>
-    <?php echo $this->loadTemplate('script'); ?>
+<?php
+echo $this->loadTemplate('footer');
+
+echo $this->loadTemplate('script');
+
