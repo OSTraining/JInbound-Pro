132833efc0d9493dbd1c4e7122ac37de0b23986c
Bill Tomczak <bill@ostraining.com>
2018-03-16 12:54:31 -0700

    Cleanup Lead Map install and view

--- src/extensions/plg_system_jinboundleadmap/jinboundleadmap.install.php
+++ src/extensions/plg_system_jinboundleadmap/jinboundleadmap.install.php
@@ -19,146 +19,128 @@ defined('JPATH_PLATFORM') or die;
 
 class plgSystemJinboundleadmapInstallerScript
 {
+    /**
+     * @param string            $type
+     * @param JInstallerAdapter $parent
+     *
+     * @return void
+     * @throws Exception
+     */
     public function postflight($type, $parent)
-    {
-        $app          = JFactory::getApplication();
-        $db           = JFactory::getDbo();
-        $table        = JTable::getInstance('menu');
-        $uninstalling = ('uninstall' == $type);
-        if (JDEBUG) {
-            $app->enqueueMessage('[' . __METHOD__ . '] Type "' . $type . '"');
-        }
-        if (!$uninstalling) {
-            $this->enable();
-        }
-        // load the parent menu item
-        $parent_id = $db->setQuery($db->getQuery(true)
-            ->select('m.id')
-            ->from('#__menu AS m')
-            ->leftJoin('#__extensions AS e ON m.component_id = e.extension_id')
-            ->where('m.parent_id = 1')
-            ->where("m.client_id = 1")
-            ->where('e.element = ' . $db->quote('com_jinbound'))
-        )->loadResult();
-        // if the parent is missing, stop unless uninstalling
-        if (empty($parent_id)) {
-            if (JDEBUG) {
-                $app->enqueueMessage('[' . __METHOD__ . '] Parent menu is empty');
-            }
-            if (!$uninstalling) {
-                return;
-            }
-        }
-        // load the existing plg_system_jinboundleadmap menu item
-        $existing = $db->setQuery($db->getQuery(true)
-            ->select('m.id')
-            ->from('#__menu AS m')
-            ->where('m.parent_id = ' . (int)$parent_id)
-            ->where("m.client_id = 1")
-            ->where("link LIKE " . $db->quote('%jinboundleadmap%'))
-        )->loadResult();
-        // if there is an existing menu, remove it
-        if ($existing) {
-            if (JDEBUG) {
-                $app->enqueueMessage('[' . __METHOD__ . '] Removing existing menu item ' . $existing);
-            }
-            if (!$table->delete((int)$existing)) {
-                $app->enqueueMessage($table->getError(), 'error');
-            }
-            $table->rebuild();
-        }
-        if ($uninstalling) {
-            return;
-        }
-        if (JDEBUG) {
-            $app->enqueueMessage('[' . __METHOD__ . '] Adding menu item');
-        }
-        $component            = $db->setQuery($db->getQuery(true)
-            ->select('e.extension_id')
-            ->from('#__extensions AS e')
-            ->where('e.element = ' . $db->quote('com_jinbound'))
-        )->loadResult();
-        $data                 = array();
-        $data['menutype']     = 'main';
-        $data['client_id']    = 1;
-        $data['title']        = 'plg_system_jinboundleadmap_view_title';
-        $data['alias']        = 'plg_system_jinboundleadmap_view_title';
-        $data['type']         = 'component';
-        $data['published']    = 0;
-        $data['parent_id']    = $parent_id;
-        $data['component_id'] = $component;
-        $data['img']          = 'class:component';
-        $data['home']         = 0;
-        $data['link']         = 'index.php?option=com_'
-            . (version_compare(JVERSION, '3.0.0', '>=') ? '' : 'jinbound&task=')
-            . 'ajax&group=system&plugin=jinboundleadmapview&format=html';
-        try {
-            // find the "leads" link in the existing menu
-            $lead_id  = $db->setQuery($db->getQuery(true)
-                ->select('id')->from('#__menu')->where('client_id = 1')
-                ->where('parent_id = ' . (int)$parent_id)
-                ->where('title = ' . $db->quote('COM_JINBOUND_LEADS'))
-            )->loadResult();
-            $location = false;
-            if ($lead_id) {
-                $location = $table->setLocation($lead_id, 'after');
-            }
-            if (false === $location) {
-                throw new Exception(JText::_('PLG_SYSTEM_JINBOUNDLEADMAP_ERROR_LOCATION_NOT_SET'));
-            }
-        } catch (Exception $ex) {
-            $app->enqueueMessage(JText::sprintf('PLG_SYSTEM_JINBOUNDLEADMAP_ERROR_INSTALLING_MENU_ITEM',
-                $ex->getMessage()), 'error');
-            return;
-        }
-        if (!$table->bind($data) || !$table->check() || !$table->store()) {
-            $app->enqueueMessage(JText::sprintf('PLG_SYSTEM_JINBOUNDLEADMAP_ERROR_INSTALLING_MENU_ITEM',
-                $table->getError()), 'error');
-        }
-        $table->rebuild();
-    }
-
-    protected function enable()
     {
         $app = JFactory::getApplication();
         $db  = JFactory::getDbo();
 
-        // find this plugin in the database, if possible...
-        $db->setQuery($db->getQuery(true)
-            ->select('extension_id')
-            ->from('#__extensions')
-            ->where($db->quoteName('element') . ' = ' . $db->quote('jinboundleadmap'))
-            ->where($db->quoteName('folder') . ' = ' . $db->quote('system'))
-            ->where($db->quoteName('type') . ' = ' . $db->quote('plugin'))
-        );
+        if (JDEBUG) {
+            $app->enqueueMessage(sprintf('[%s] Type "%s"', __METHOD__, $type));
+        }
 
-        try {
-            $eid = $db->loadResult();
-            if (!$eid) {
-                throw new Exception('Could not enable plugin! ' . __METHOD__);
-            }
-        } catch (Exception $e) {
-            if (defined('JDEBUG') && JDEBUG) {
-                $app->enqueueMessage(htmlspecialchars($e->getMessage()));
-            }
+        // load the parent menu item
+        $parentId = $db->setQuery(
+            $db->getQuery(true)
+                ->select('m.id')
+                ->from('#__menu AS m')
+                ->leftJoin('#__extensions AS e ON m.component_id = e.extension_id')
+                ->where(
+                    array(
+                        'm.parent_id = 1',
+                        'm.client_id = 1',
+                        'e.element = ' . $db->quote('com_jinbound')
+                    )
+                )
+        )
+            ->loadResult();
+
+        if (!$parentId) {
             return;
         }
 
-        // force-enable this plugin
-        $db->setQuery($db->getQuery(true)
-            ->update('#__extensions')
-            ->set($db->quoteName('enabled') . ' = 1')
-            ->where($db->quoteName('extension_id') . ' = ' . (int)$eid)
+        $existing = $db->setQuery(
+            $db->getQuery(true)
+                ->select('m.id')
+                ->from('#__menu AS m')
+                ->where(
+                    array(
+                        'm.parent_id = ' . (int)$parentId,
+                        'm.client_id = 1',
+                        'link LIKE ' . $db->quote('%jinboundleadmap%')
+                    )
+                )
+        )
+            ->loadResult();
+
+        if ($existing) {
+            if (JDEBUG) {
+                $app->enqueueMessage('[' . __METHOD__ . '] Removing existing menu item ' . $existing);
+            }
+
+            /** @var JTableMenu $table */
+            $table = JTable::getInstance('menu');
+            if (!$table->delete((int)$existing)) {
+                $app->enqueueMessage($table->getError(), 'error');
+            }
+
+            $table->rebuild();
+        }
+
+        if (JDEBUG) {
+            $app->enqueueMessage('[' . __METHOD__ . '] Adding menu item');
+        }
+        $component = $db->setQuery(
+            $db->getQuery(true)
+                ->select('e.extension_id')
+                ->from('#__extensions AS e')
+                ->where('e.element = ' . $db->quote('com_jinbound'))
+        )
+            ->loadResult();
+        $data      = array(
+            'menutype'     => 'main',
+            'client_id'    => 1,
+            'title'        => 'plg_system_jinboundleadmap_view_title',
+            'alias'        => 'plg_system_jinboundleadmap_view_title',
+            'type'         => 'component',
+            'published'    => 0,
+            'parent_id'    => $parentId,
+            'component_id' => $component,
+            'home'         => 0,
+            'link'         => 'index.php?option=com_ajax&group=system&plugin=jinboundleadmapview&format=html'
         );
 
         try {
-            $db->query();
-        } catch (Exception $e) {
-            if (defined('JDEBUG') && JDEBUG) {
-                $app->enqueueMessage(htmlspecialchars($e->getMessage()));
+            // find the "leads" link in the existing menu
+            $leadId = $db->setQuery(
+                $db->getQuery(true)
+                    ->select('id')
+                    ->from('#__menu')
+                    ->where(
+                        array(
+                            'client_id = 1',
+                            'parent_id = ' . (int)$parentId,
+                            'title = ' . $db->quote('COM_JINBOUND_LEADS')
+                        )
+                    )
+            )
+                ->loadResult();
+
+            if ($leadId) {
+                $table->setLocation($leadId, 'after');
             }
+
+            if (!$table->bind($data) || !$table->check() || !$table->store()) {
+                throw new Exception(
+                    JText::sprintf('PLG_SYSTEM_JINBOUNDLEADMAP_ERROR_INSTALLING_MENU_ITEM', $table->getError()),
+                    500
+                );
+            }
+
+            $table->rebuild();
+
+        } catch (Exception $e) {
+            $app->enqueueMessage(
+                JText::sprintf('PLG_SYSTEM_JINBOUNDLEADMAP_ERROR_INSTALLING_MENU_ITEM', $e->getMessage()),
+                'error'
+            );
             return;
         }
-
     }
 }
--- src/extensions/plg_system_jinboundleadmap/tmpl/default.php
+++ src/extensions/plg_system_jinboundleadmap/tmpl/default.php
@@ -15,89 +15,114 @@
  * may be added to this header as long as no information is deleted.
  */
 
+use Joomla\Utilities\ArrayHelper;
+
 defined('JPATH_PLATFORM') or die;
+
 
 ?>
 <div class="container-fluid" id="jinbound_component">
-    <?php if (!empty($this->sidebar)) : ?>
-    <div id="j-sidebar-container" class="span2">
-        <?php echo $this->sidebar; ?>
-    </div>
-    <div id="j-main-container" class="span10">
-        <?php else : ?>
-        <div id="j-main-container">
-            <?php endif; ?>
-            <?php if ($this->data instanceof Exception) : ?>
-                <div class="alert alert-error">
-                    <?php echo $this->data->getMessage(); ?>
-                    <?php if (JDEBUG) : ?>
-                        <pre><?php echo $this->data->getTraceAsString(); ?></pre>
-                    <?php endif; ?>
-                </div>
-            <?php elseif (empty($this->data->locations)) : ?>
-                <div class="alert alert-error">
-                    <?php echo JText::_('PLG_SYSTEM_JINBOUNDLEADMAP_NO_LOCATIONS_FOUND'); ?>
-                </div>
-            <?php echo $this->loadTemplate('filterbar'); ?>
-            <?php else : ?>
-                <div id="map"></div>
-                <div id="legend">
-                    <div id="legend-container"><h3><?php echo JText::_('PLG_SYSTEM_JINBOUNDLEADMAP_LEGEND'); ?></h3>
-                    </div>
-                </div>
-                <script type="text/javascript">
-                    function mapReady(f) {
-                        /in/.test(document.readyState) ? setTimeout('mapReady(' + f + ')', 9) : f()
-                    }
-
-                    mapReady(function() {
-                        var latLngs = <?php echo json_encode($this->data->locations); ?>;
-                        var centerLatLng = {lat: 30, lng: 0};
-                        var legend = document.getElementById('legend');
-                        var legendContainer = document.getElementById('legend-container');
-                        var guestLegend = document.createElement('div');
-                        var leadLegend = document.createElement('div');
-
-                        var map = new google.maps.Map(document.getElementById('map'), {
-                            zoom  : 2,
-                            center: centerLatLng
-                        });
-
-                        var guestIcon = new StyledIcon(StyledIconTypes.MARKER, {color: "#0000ff"});
-                        var leadIcon = new StyledIcon(StyledIconTypes.MARKER, {color: "#ff0000"});
-
-                        guestLegend.innerHTML = '<img src="' + guestIcon.icon + '" /> <?php echo JText::_('PLG_SYSTEM_JINBOUNDLEADMAP_LEGEND_GUEST'); ?>';
-                        leadLegend.innerHTML = '<img src="' + leadIcon.icon + '" /> <?php echo JText::_('PLG_SYSTEM_JINBOUNDLEADMAP_LEGEND_LEAD'); ?>';
-
-                        legendContainer.appendChild(guestLegend);
-                        legendContainer.appendChild(leadLegend);
-
-                        map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(legend);
-
-                        for (var i = 0, n = latLngs.length; i < n; i++) {
-                            var marker = new StyledMarker({
-                                position : {lat: latLngs[i].latitude, lng: latLngs[i].longitude},
-                                map      : map,
-                                title    : latLngs[i].city,
-                                styleIcon: latLngs[i].lead ? leadIcon : guestIcon
-                            });
-                            if (latLngs[i].lead) {
-                                marker._jib_lead = latLngs[i].lead;
-                                marker.addListener('click', function() {
-                                    window.location.href = 'index.php?option=com_jinbound&task=contact.edit&id=' + this._jib_lead;
-                                });
-                            }
-                        }
-                    });
-                </script>
-                <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-                <script type="text/javascript" src="../media/jinboundleadmap/js/StyledMarker.js"></script>
-            <?php endif; ?>
-            <div class="alert alert-success">
-                <p><?php echo JText::sprintf('PLG_SYSTEM_JINBOUNDLEADMAP_UPDATE_MAXMIND_DB',
-                        $this->download_url); ?></p>
-                <p>This product includes GeoLite2 data created by MaxMind, available from <a
-                        href="http://www.maxmind.com">http://www.maxmind.com</a>.</p>
+    <?php
+    $mainAttribs = array(
+        'id' => 'j-main-container'
+    );
+    if (!empty($this->sidebar)) :
+        $mainAttribs['class'] = 'span10';
+        ?>
+        <div id="j-sidebar-container" class="span2">
+            <?php echo $this->sidebar; ?>
+        </div>
+    <?php
+    endif;
+    ?>
+    <div <?php echo ArrayHelper::toString($mainAttribs); ?>>
+        <?php
+        if ($this->data instanceof Exception) :
+            ?>
+            <div class="alert alert-error">
+                <?php echo $this->data->getMessage(); ?>
+                <?php
+                if (JDEBUG) :
+                    ?>
+                    <pre><?php echo $this->data->getTraceAsString(); ?></pre>
+                <?php
+                endif;
+                ?>
             </div>
+        <?php
+        elseif (empty($this->data->locations)) :
+        ?>
+            <div class="alert alert-error">
+                <?php echo JText::_('PLG_SYSTEM_JINBOUNDLEADMAP_NO_LOCATIONS_FOUND'); ?>
+            </div>
+        <?php
+        echo $this->loadTemplate('filterbar');
+
+        else :
+        ?>
+            <div id="map"></div>
+            <div id="legend">
+                <div id="legend-container"><h3><?php echo JText::_('PLG_SYSTEM_JINBOUNDLEADMAP_LEGEND'); ?></h3>
+                </div>
+            </div>
+            <script type="text/javascript">
+                function mapReady(f) {
+                    /in/.test(document.readyState) ? setTimeout('mapReady(' + f + ')', 9) : f()
+                }
+
+                mapReady(function() {
+                    var latLngs = <?php echo json_encode($this->data->locations); ?>;
+                    var centerLatLng = {lat: 30, lng: 0};
+                    var legend = document.getElementById('legend');
+                    var legendContainer = document.getElementById('legend-container');
+                    var guestLegend = document.createElement('div');
+                    var leadLegend = document.createElement('div');
+
+                    var map = new google.maps.Map(document.getElementById('map'), {
+                        zoom  : 2,
+                        center: centerLatLng
+                    });
+
+                    var guestIcon = new StyledIcon(StyledIconTypes.MARKER, {color: "#0000ff"});
+                    var leadIcon = new StyledIcon(StyledIconTypes.MARKER, {color: "#ff0000"});
+
+                    guestLegend.innerHTML = '<img src="' + guestIcon.icon + '" /> <?php echo JText::_('PLG_SYSTEM_JINBOUNDLEADMAP_LEGEND_GUEST'); ?>';
+                    leadLegend.innerHTML = '<img src="' + leadIcon.icon + '" /> <?php echo JText::_('PLG_SYSTEM_JINBOUNDLEADMAP_LEGEND_LEAD'); ?>';
+
+                    legendContainer.appendChild(guestLegend);
+                    legendContainer.appendChild(leadLegend);
+
+                    map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(legend);
+
+                    for (var i = 0, n = latLngs.length; i < n; i++) {
+                        var marker = new StyledMarker({
+                            position : {lat: latLngs[i].latitude, lng: latLngs[i].longitude},
+                            map      : map,
+                            title    : latLngs[i].city,
+                            styleIcon: latLngs[i].lead ? leadIcon : guestIcon
+                        });
+                        if (latLngs[i].lead) {
+                            marker._jib_lead = latLngs[i].lead;
+                            marker.addListener('click', function() {
+                                window.location.href = 'index.php?option=com_jinbound&task=contact.edit&id=' + this._jib_lead;
+                            });
+                        }
+                    }
+                });
+            </script>
+            <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+            <script type="text/javascript" src="../media/jinboundleadmap/js/StyledMarker.js"></script>
+        <?php
+        endif;
+        ?>
+        <div class="alert alert-success">
+            <p>
+                <?php
+                echo JText::sprintf('PLG_SYSTEM_JINBOUNDLEADMAP_UPDATE_MAXMIND_DB', $this->download_url);
+                ?>
+            </p>
+            <p>This product includes GeoLite2 data created by MaxMind, available from
+                <a href="http://www.maxmind.com">http://www.maxmind.com</a>.</p>
         </div>
     </div>
+</div>
