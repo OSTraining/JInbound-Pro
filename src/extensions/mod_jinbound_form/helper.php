cc0dacaa0c79b640f21e19dbb3a8471d4abf81e8
Bill Tomczak <bill@ostraining.com>
2018-09-27 11:10:18 -0700

    Use module title for page name JIN-22

--- src/extensions/mod_jinbound_form/helper.php
+++ src/extensions/mod_jinbound_form/helper.php
@@ -125,7 +125,7 @@ abstract class modJinboundFormHelper
         return (object)array(
             'campaign_id'         => $campaign_id,
             'form_id'             => $form_id,
-            'page_name'           => preg_replace('/^mod_/', '', $module->module),
+            'page_name'           => $module->title,
             'notification_email'  => $params->get('notification_email', ''),
             'after_submit_sendto' => $params->get('after_submit_sendto', 'message'),
             'menu_item'           => $params->get('menu_item', ''),
